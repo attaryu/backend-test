3b0a009b28102085b71599537918d3470a8f7c19
attaryu <mattarannaufal@gmail.com>
2024-01-15 00:13:30 +0700

    lesson endpoint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,11 +3,13 @@ import { Router } from 'express';
 import lectureRoutes from './lecture.route.js';
 import classRoutes from './class.route.js';
 import studentRoutes from './student.route.js';
+import lessonRoutes from './lesson.route.js';
 
 const routes = Router();
 
 routes.use(lectureRoutes);
 routes.use(classRoutes);
 routes.use(studentRoutes);
+routes.use(lessonRoutes);
 
 export default routes;
